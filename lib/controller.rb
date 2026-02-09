a8cff736c7419cb5d4971ace5f820fb79161ee1c
crousty24-bit <allen.kch24@gmail.com>
2026-02-09 15:00:14 +0100

    feat: Add class Gossip to get user new gossip & save to CSV file
    update MVC structure; add dir db, app/views
    
    run pry test to validate save Gossip method
    
    add GET & POST path for new gossips form

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < Sinatra::Base # new Class Application herit of class Sinatra (from gem)
-  get '/' do
-    erb :index
+  get '/gossips/new/' do #display the given url page
+    erb :new_gossip
+  end
+  post '/gossips/new/' do #send data entered by user in form
+  puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
   end
 end
---
+++ lib/gossip.rb
@@ -0,0 +1,15 @@
+require 'csv'
+#require 'pry'
+class Gossip
+  attr_reader :author, :content
+  def initialize(author, content)
+    @author = author
+    @content= content
+  end
+  def save
+    CSV.open("db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+end
+#binding.pry
